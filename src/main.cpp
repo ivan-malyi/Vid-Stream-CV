1f26df0a8836b300adeb75beed65ad2f1660c4eb
Ivan <ivanmalyj1994@gmail.com>
2025-04-16 15:28:39 +0300

    Project init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+int main(){
+
+    std::cout<< "Vid Stream Cv Project"<<std::endl;
+
+
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "src/main.cpp"
+
+
+int main() {
+    std::cout<<"Let`s test you`re project!"<<std::endl;
+    return 0;
+}

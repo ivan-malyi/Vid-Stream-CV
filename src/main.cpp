e3b6f569866b604bae7cb12dff29890726bfb6e7
Fta1d <li4nomark228@gmail.com>
2025-04-18 13:41:28 +0300

    Added convertation from mat obj to gst buffer to gst sample

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,55 @@
 
 using namespace cv;
 
+GstBuffer *mat_to_gst_buffer(const Mat &frame) {
+    gsize size = frame.step[0] * frame.rows;
+
+    GstBuffer *buffer = gst_buffer_new_allocate(NULL, size, NULL);
+
+    if (!buffer) {
+        std::cout << "Couldn't create buffer!" << std::endl;
+        return nullptr;
+    }
+
+    GstMapInfo map;
+
+    if (!gst_buffer_map(buffer, &map, GST_MAP_WRITE)) {
+        std::cout << "Couldn't map buffer!" << std::endl;
+        return nullptr;
+    }
+
+    if (frame.isContinuous()) {
+        memcpy(map.data, frame.data, size);
+    } else {
+        size_t row_size = frame.cols * frame.elemSize();
+        for (int i = 0; i < frame.rows; ++i) {
+            memcpy(map.data + i * row_size, frame.ptr(i), row_size);
+        }
+    }
+
+    gst_buffer_unmap(buffer, &map);
+    return buffer;
+}
+
+GstSample *gst_buffer_to_gst_sample(const Mat &frame, GstCaps *caps) {
+    GstBuffer *buffer = mat_to_gst_buffer(frame);
+
+    if (!buffer) {
+        return nullptr;
+    }
+
+    GstClockTime timestamp = gst_util_get_timestamp();
+
+    GST_BUFFER_PTS(buffer) = timestamp;
+    GST_BUFFER_DTS(buffer) = timestamp;
+
+    GstSample *sample = gst_sample_new(buffer, caps, NULL, NULL);
+
+    gst_buffer_unref(buffer);
+
+    return sample;
+}
+
 Mat gst_sample_to_mat(GstSample* sample) {
     GstBuffer* buffer = gst_sample_get_buffer(sample);
     GstMapInfo map;
@@ -59,11 +108,20 @@ Mat gst_sample_to_mat(GstSample* sample) {
 int main(int argc, char** argv) {
     gst_init(&argc, &argv);
 
+
+    // Src pipeline
     GstElement* pipeline = gst_pipeline_new("pipeline");
     GstElement* source = gst_element_factory_make("v4l2src", "source");
     GstElement* convert = gst_element_factory_make("videoconvert", "convert");
     GstElement* sink = gst_element_factory_make("appsink", "sink");
+
+    // Dst pipeline
+    GstElement* payloader = gst_element_factory_make("rtph264pay", "payloader");
+    GstElement* udpsink = gst_pipeline_new("udpsink", "udpsink");
     
+    g_object_set(G_OBJECT(udpsink), "host", "192.168.1.100", NULL);
+    g_object_set(G_OBJECT(udpsink), "port", 5000, NULL);
+
     if (!pipeline || !source || !convert || !sink) {
         std::cerr << "Couldn't create gstreamer elements." << std::endl;
         return -1;
@@ -73,7 +131,7 @@ int main(int argc, char** argv) {
     g_object_set(G_OBJECT(sink), "drop", TRUE, NULL);
 
     GstCaps* caps = gst_caps_new_simple("video/x-raw",
-                                        "format", G_TYPE_STRING, "BGR",
+                                        "format", G_TYPE_STRING, "RGB",
                                         NULL);
     gst_app_sink_set_caps(GST_APP_SINK(sink), caps);
     gst_caps_unref(caps);
@@ -81,7 +139,7 @@ int main(int argc, char** argv) {
     gst_bin_add_many(GST_BIN(pipeline), source, convert, sink, NULL);
 
     if (!gst_element_link_many(source, convert, sink, NULL)) {
-        std::cerr << "Couldn't cat gstreamer elements" << std::endl;
+        std::cerr << "Couldn't link gstreamer elements" << std::endl;
         return -1;
     }
 
